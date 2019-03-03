8aba6d8d65bfd7a58913e051a063421c47f5a8a5
drupalspec <drupalspec@gmail.com>
2019-03-03 22:24:52 +0200

    ADD

--- js/main.js
+++ js/main.js
@@ -165,4 +165,13 @@ window.onload = function(){
 };
 //add an event binder to the button
 addButton.addEventListener('click',addToList);
+addInput.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    addToList();
+  }
+});
 clearButton.addEventListener('click',clearList);
