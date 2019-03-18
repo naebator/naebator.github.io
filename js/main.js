5962f8f38feaefa426fd3b69d844682e4c64b958
Roman Golubov <roman@obscuredev.net>
2019-03-18 20:33:28 +0200

    UPDT

--- js/main.js
+++ js/main.js
@@ -1,177 +1,189 @@
-//Todo list app by Afolabi Sheriff
-//features
-//store in localstorage of browser
-//delete list items
+/* Shopping calculator app by Roman Golubov
+ * features:
+ * calculate total itemst in card
+ * store data in LolcalStorage
+ * remove item from list
+ * add item quantity
+*/
 
+// DOM variables
+const addBtn = document.querySelector('.addButton');
+const formValue = document.querySelector('.itemInput');
+const totalPrice = document.querySelector('.total');
+const list = document.querySelector(".itemList");
+let dataList = [];
 
-var addButton = document.getElementById('addButton');
-var addInput = document.getElementById('itemInput');
-var todoList = document.getElementById('todoList');
-var cradTotal = document.getElementById('total');
-var listArray = [];
+// Init Event Listeners
+addBtn.addEventListener('click', addElement);
+formValue.addEventListener("keyup", submitOnEnter);
 
-//declare addToList function
-
-function numberFormatter(string) {
-    string = parseFloat(string.replace(',','.').replace(' ',''))
-    return string;
+// Init functions
+// Model Class
+class Model {
+	constructor (value, quantity = 1) {
+		this.value = value;
+		this.defaultValue = value;
+		this.quantity = quantity;
+		this.id = this.generateId();
+	}
+	// generate Id method
+	generateId () {
+		return '_' + Math.random().toString(36).substr(2, 9);
+	}
 }
 
-function listItemObj(content, status) {
-    this.content = '';
-    this.status = 'incomplete';
-}
-var changeToComp = function(){
-    var parent = this.parentElement;
-    console.log('Changed to complete');
-    parent.className = 'uncompleted';
-    this.innerText = 'Incomplete';
-    this.removeEventListener('click',changeToComp);
-    this.addEventListener('click',changeToInComp);
-    changeListArray(parent.firstChild.innerText,'complete');
+// load data from localStorage
+window.onload = function() {
+	let list = localStorage.getItem('dataList');
 
-}
+	// check of list not empty
+	if (list != null) {
+		list = JSON.parse(list);
+		dataList = list;
 
-var changeToInComp = function(){
-    var parent = this.parentElement;
-    console.log('Changed to incomplete');
-    parent.className = 'completed';
-    this.innerText = 'Complete';
-    this.removeEventListener('click',changeToInComp);
-    this.addEventListener('click',changeToComp);
+		// update dom from localStorage
+		list.forEach(x => updateDom(x, reload = true));
 
-    changeListArray(parent.firstChild.innerText,'incomplete');
-
+		// make total calculation
+	    return calculateTotal(list);	
+	}
 }
 
-var removeItem = function(){
-    var parent = this.parentElement.parentElement;
-    parent.removeChild(this.parentElement);
-    var data = this.parentElement.firstChild.innerText;
 
-
-    cradTotal.innerText = (numberFormatter(cradTotal.innerText) - numberFormatter(data)).toFixed(2);
-    if(listArray.length == 1) {
-        clearButton.style.display = "none";
-    }
-
-    for(var i=0; i < listArray.length; i++){
-
-        if(listArray[i].content == data){
-            listArray.splice(i,1);
-            refreshLocal();
-            break;
-        }
-    }
-
-
-}
-
-//function to change the todo list array
-var changeListArray = function(data,status){
-
-    for(var i=0; i < listArray.length; i++){
-
-        if(listArray[i].content == data){
-            listArray[i].status = status;
-            refreshLocal();
-            break;
-        }
-    }
-}
-
-//function to chage the dom of the list of todo list
-var createItemDom = function(text,status){
-
-    var listItem = document.createElement('li');
-
-    var itemLabel = document.createElement('label');
-
-    var itemIncompBtn = document.createElement('button');
-
-    listItem.className = (status == 'incomplete')?'completed':'uncompleted';
-
-    itemLabel.innerText = numberFormatter(text).toFixed(2);
-    // здесь
-
-    itemIncompBtn.className = 'btn btn-sm btn-danger';
-    itemIncompBtn.innerText = '–';
-    itemIncompBtn.addEventListener('click',removeItem);
-    listItem.appendChild(itemLabel);
-    listItem.appendChild(itemIncompBtn);
-
-    return listItem;
-}
-
-var refreshLocal = function(){
-    var todos = listArray;
-    localStorage.removeItem('todoList');
-    localStorage.setItem('todoList', JSON.stringify(todos));
-}
-
-var refreshTotal = function(data) {
-    return cradTotal.innerText = (numberFormatter(cradTotal.innerText) + numberFormatter(data)).toFixed(2);
-}
-
-var addToList = function(){
-    if(!addInput.value) {
+// add new element to list
+function addElement() {
+	// chek if input not empty
+	if(!formValue.value) {
         return;
     }
-    var newItem = new listItemObj();
 
-    newItem.content = numberFormatter(addInput.value).toFixed(2);
+    // create New element
+    let newElement = new Model(+formValue.value);
 
-    refreshTotal(newItem.content);
-    listArray.unshift(newItem);
-    //add to the local storage
-    refreshLocal();
-    //change the dom
-    var item = createItemDom(addInput.value,'incomplete');
-    todoList.insertBefore(item, todoList.firstChild);
-    addInput.value = '';
+    // add value to list
+    dataList.unshift(newElement);
 
-    if(listArray.length > 4) {
-        clearButton.style.display = "block";
-    }
+    // update DOM
+    updateDom(newElement, reload = false);
+
+    // update LocalStorage
+    updateLocalStorage();
+
+    // plus one Item to total Price
+    addEl(+formValue.value);
+	
+	// reset value
+    return formValue.value = '';
 }
 
-//function to clear todo list array
-var clearList = function(){
-    listArray = [];
-    localStorage.removeItem('todoList');
-    todoList.innerHTML = '';
-    clearButton.style.display = "none";
-    cradTotal.innerText = 0.00;
-
+// update localStorage
+function updateLocalStorage () {
+	return localStorage.setItem('dataList', JSON.stringify(dataList));
 }
 
-window.onload = function(){
-    var list = localStorage.getItem('todoList');
 
-    if (list != null) {
-        todos = JSON.parse(list);
-        listArray = todos;
+// update DOM
+function updateDom(el) {
+	// create elements in DOM
+	const li = document.createElement("li");
+	const label = document.createElement('label');
+	const buttonMinus  = document.createElement('button');
+	let buttonPlus  = document.createElement('button');
 
-        for(var i=0; i<listArray.length;i++){
-            var data = listArray[i].content;
-            refreshTotal(data);
+	// set item price
+	label.innerText = el.value;
 
-            var item = createItemDom(data,listArray[i].status);
-            todoList.appendChild(item);
-        }
+	// set delete btn
+	buttonMinus.className = 'btn btn-sm btn-danger';
+    buttonMinus.innerText = '–';
+    buttonMinus.addEventListener('click', removeEl);
 
-    }
+    // set plus button
+    buttonPlus.className = 'btn btn-sm btn-success';
+    buttonPlus.innerText = '+';
+    buttonPlus.addEventListener('click', plusOne);
 
-};
-//add an event binder to the button
-addButton.addEventListener('click',addToList);
-addInput.addEventListener("keyup", function(event) {
+    //set ID to element
+    li.dataset.id = el.id;
+
+	// add elements to DOM
+	if(reload) {
+		list.appendChild(li);
+	} else {
+		list.insertBefore(li, list.firstChild);
+	}
+	li.appendChild(label);
+	li.appendChild(buttonPlus);
+	li.appendChild(buttonMinus);
+}
+
+// calculate total price
+function calculateTotal(list) {
+	let total = 0;
+	list.forEach(x => total += x.value);
+	return totalPrice.innerText = total.toFixed(2);
+}
+
+// add element to the list
+function addEl(el) {
+	let formatEl = el.toFixed(2);
+	return totalPrice.innerText = (+totalPrice.innerText + +formatEl).toFixed(2);
+}
+
+// remove Element from the list
+function removeEl() {
+	// total calculation
+	let curPrice = +this.parentElement.firstChild.innerText;
+	totalPrice.innerText = (+totalPrice.innerText - curPrice).toFixed(2);
+
+	// remove element from DOM
+	let parent = this.parentElement.parentElement;
+	parent.removeChild(this.parentElement);
+
+	// remove element from data Array
+	let id = this.parentElement.dataset.id;
+	dataList = dataList.filter(x => x.id !== id);
+
+	// update LocalStorage
+	return updateLocalStorage();
+}
+
+// pluse One element with the same price
+function plusOne() {
+	console.log(this);
+	let id = this.parentElement.dataset.id;
+
+	// find current element;
+	index = dataList.findIndex((x => x.id == id));
+
+	// change quantity += + 1;
+	dataList[index]['quantity'] += 1;
+
+	// reCalculate current price
+	this.parentElement.firstChild.innerText = 
+		dataList[index]['defaultValue'] * dataList[index]['quantity'];
+
+	// reCalculate total price
+	addEl(dataList[index]['value']);
+
+	// update data Array
+	dataList[index]['value'] = dataList[index]['defaultValue'] * dataList[index]['quantity'];
+
+	// update LocalStorage
+	updateLocalStorage();
+
+	// make total calculation
+    return calculateTotal(dataList);
+}
+
+
+// submit result on Enter
+function submitOnEnter(event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
     // Cancel the default action, if needed
     event.preventDefault();
     // Trigger the button element with a click
-    addToList();
+    return addElement();
   }
-});
-clearButton.addEventListener('click',clearList);
+}
